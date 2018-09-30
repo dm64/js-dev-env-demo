6ab77a6e55043357658443817c85ec63a5c7669d
dm64 <34005146+dm64@users.noreply.github.com>
2018-09-30 09:23:49 +0100

    deploy to heroku

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://secure-tor-83326.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
